fcf9404de8a42352b2907b3838c9cb18f8ae3210
shyamkrishnank <shyamkrishnank18@gmail.com>
2023-05-02 16:19:48 +0530

    new file added

---
+++ quickSort.py
@@ -0,0 +1,20 @@
+def pivot(arr,pivot_index,end_index):
+   swap_index = pivot_index
+   for i in range(pivot_index+1,end_index+1):
+       if arr[i] < arr[pivot_index]:
+           swap_index += 1
+           arr[i],arr[swap_index] = arr[swap_index],arr[i]
+   arr[pivot_index],arr[swap_index] = arr[swap_index],arr[pivot_index]
+   return swap_index
+def quick(arr,left,right):
+   if left < right
+       pivot_index = pivot(arr,left,right)
+       left = quick(arr,left,pivot_index-1)
+       right = quick(arr,pivot_index+1,right)
+       return arr
+
+def quick_sort(arr):
+    return quick(arr,0,len(arr)-1)
+
+def quick_sort(arr):
+   return quick(arr,0,len(arr)-1)
